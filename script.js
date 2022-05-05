643c9be3b52bd0e3d07603e0dd18a89a0a1e17f2
JEREMY_QIN <jeremyqin1@gmail.com>
2022-05-05 17:29:22 -0400

    updates

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function faireDisparaitre() {
+    /* votre code pour le faux loading ici!*/
+    var loading = document.getElementById("fauxloading");
+    loading.style.display = "none";
+}
 
+
+//NE RIEN MODIFIER EN DESSOUS DE CETTE LIGNE!!
+function attendreAvantDeDisparaitre() {
+          setTimeout(function () {
+            faireDisparaitre();
+
+          },
+          JSON.parse(data)[0].showFakeLoading == 0? 0: 5000);
+
+}
+attendreAvantDeDisparaitre();
+
+
+/* montrer le toast quand on load la page */
+window.onload = (event) => {
+  var toastLiveExample = document.getElementById('liveToast')
+  var toast = new bootstrap.Toast(toastLiveExample)
+  
+  toast.show()
+}
